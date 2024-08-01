15c6f033afc6d5d27869cc443b79b8ea6008c11f
Manuel <manuelhany@gmail.com>
2024-08-01 12:21:14 +0300

    setup Django Admin

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,49 @@
-from django.contrib import admin  # noqa
+"""
+Django admin customization.
+"""
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from . import models
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users."""
+    # Here we basically customize the field sets variables
+    # And we have only specified fields that exist in our models
+    # and exluded fields which do not exist in the default one such as the username field
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',), # wide is enabling a css class which beautify the view a little bit
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            )
+        }),
+    )
+
+
+# Here we basically register the User model with the specification we specified in our UserAdmin class
+admin.site.register(models.User, UserAdmin)
